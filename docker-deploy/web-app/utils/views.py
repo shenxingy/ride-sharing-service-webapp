750e2fa82c8de999c904a0082fc945f2d0a949fa
jh730 <jh730@duke.edu>
2025-02-06 16:14:21 -0500

    Update file views.py

--- docker-deploy/web-app/utils/views.py
+++ docker-deploy/web-app/utils/views.py
@@ -16,7 +16,7 @@ def start_oauth_flow(request):
     request.session['oauth_state'] = state
     return redirect(authorization_url)
 
-def oauth2_callback(request):wo
+def oauth2_callback(request):
     try:
         logger.info("OAuth callback received")
         
